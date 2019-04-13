cb463445dd0736634bde67bc2cf3e30014bcb380
crevasse02 <wiboworaffi82@gmail.com>
2019-04-13 17:38:28 +0700

    Fix conflict

--- cms/application/config/config.php
+++ cms/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/pemweb/projectUAS';
+$config['base_url'] = 'localhost/UAS_PEMWEB';
 
 /*
 |--------------------------------------------------------------------------
